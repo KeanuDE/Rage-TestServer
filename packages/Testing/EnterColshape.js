b12203998d3ace231911b8d74805f7b5bfb09e1c
KeanuLox <skyloxkeanu@gmail.com>
2020-05-15 02:48:45 +0200

    colshape

---
+++ packages/Testing/EnterColshape.js
@@ -0,0 +1,23 @@
+/*
+var enterColshape = mp.colshapes.newRectangle(x, y, width, height)
+mp.events.add("playerEnterColshape",(player,shape) => {
+  if(shape == enterColshape) {
+    player.position = new mp.Vector3(x,y,z);
+  }
+})
+*/
+
+/*
+var hurensohn = mp.colshapes.newRectangle(0,0,1,2);
+
+mp.markers.new(27,new mp.Vector3(0,0,0),1);
+*/
+
+
+mp.events.add("playerEnterColshape",(player,shape) => {
+  /*if(shape == hurensohn) {
+    player.position = new mp.Vector3(-1505.783, -3012.587, -80.000);
+  }*/
+
+
+})
